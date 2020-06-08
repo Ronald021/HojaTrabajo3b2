d5a584b0e8e29102f45fc176fc438c5025237440
Ronald <ottoniel.21@outlook.es>
2020-06-07 18:11:11 -0600

    Versión inicial de la Hoja de trabajo 3

---
+++ miPrograma1/Properties/view/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using miPrograma1.Properties.service;
+
+namespace miPrograma1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            convert hi = new convert();
+            hi.create(args);
+            
+        }
+    }
+}

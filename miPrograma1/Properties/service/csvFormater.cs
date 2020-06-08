377013975c6d6cb5ec425d71f2abfb598e7d33e2
Ronald <ottoniel.21@outlook.es>
2020-06-07 18:08:49 -0600

    Versión inicial de la Hoja de trabajo 3

---
+++ miPrograma1/Properties/service/csvFormater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace miPrograma1.Properties.service
+{
+    public class convert
+    {
+        public void create(String[] archivo)
+        {
+  
+            float a = archivo.Length;
+            for (int c = 0; c < a; c++)
+            {
+                string text = System.IO.File.ReadAllText(@archivo[c]);
+                text= text.Replace(" ", ",");
+                System.Console.WriteLine("Archivo No. : " +(c+1));
+                System.Console.WriteLine(text+"\n");
+                
+            }
+
+        } 
+    }
+}
+
